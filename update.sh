58658340ba7899bc9a690bb04bd4517b5d5021e7
Retr0 <pentest.lamusy@gmail.com>
2019-01-10 13:38:00 -0500

    added av scan

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 echo "time to update your debian and clean it :)"
 apt update
-apt install -fs
-apt upgrade -y
+apt install -fsy > /dev/null
+apt upgrade -y > /dev/null
 apt dist-upgrade -y
 apt autoremove -y
 apt autoclean
+clamscan -q --recursive --remove --detect-pua=yes /
 echo "done"
