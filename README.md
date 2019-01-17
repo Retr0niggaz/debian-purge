035de8ae09a7360f197ea90a0a530df89b0098d5
Retr0 <pentest.lamusy@gmail.com>
2019-01-17 14:27:19 -0500

    version 1.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# debian purge 
+# debian purge
 # little (made in python) script for just installed debian
-# and 2nd script in bash to update clean and purge your system
+# and 2nd script in bash to update clean and purge your system including antivirus scan
 # requirments Python2
 # ! RUN SCRIPT AS ROOT !
--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 echo "time to update your debian and clean it :)"
-apt update
-apt install -fsy > /dev/null
-apt upgrade -y > /dev/null
-apt dist-upgrade -y
-apt autoremove -y
-apt autoclean
-clamscan -q --recursive --remove --detect-pua=yes /
+sudo dpkg --add-architecture i386
+sudo apt update > /dev/null
+sudo apt install -fy
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove
+sudo apt autoclean
+sudo apt clean
+sudo freshclam
+sudo clamscan --quiet --recursive --remove --detect-pua=yes /
 echo "done"
+echo "enjoy"
