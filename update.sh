5310205b5a0e833abc4ec626d54bd349b33e723c
root <root@laptopik>
2019-02-12 14:04:24 +0100

    bugs fixed

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 echo "time to update your debian and clean it :)"
-python2 debian-purge.py
 sudo dpkg --add-architecture i386
 sudo apt update > /dev/null
 sudo apt install -fy
@@ -9,7 +8,7 @@ sudo apt dist-upgrade -y
 sudo apt autoremove
 sudo apt autoclean
 sudo apt clean
-sudo freshclam
-sudo clamscan --quiet --recursive --remove --detect-pua=yes /
+sudo freshclam > /dev/null
+sudo clamscan --quiet --recursive --remove --detect-pua=yes / &
 echo "done"
 echo "enjoy"
