914440148f1d7754d088c4d66030aa23dda32c84
elliot <elliot@gateway>
2019-08-30 18:03:55 +0200

    bugs fixed and code purged

--- debian-purge.py
+++ debian-purge.py
@@ -1,37 +1,57 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import os, time
+
+def c():
+    os.system("clear")
+
 def debian():
-    print('\033[93mRUN AS ROOT\033[0m')
-    print('first time to setup sudo (run as root)')
-    name = raw_input('give the name of the user you want to add to the sudo group (type nothing to continue): ')
+    if os.getuid() != 0:
+        c()
+        print('\033[91mRUN AS ROOT\033[0m')
+        exit()
+    print('first time to setup sudo')
+
+    name = input('who u want to add to sudo group (press enter with nothing to continue): ')
     if name != "":
-        os.system('adduser %s sudo' %name)
-    print('adding now repositories with more software')
-    os.system('rm /etc/apt/sources.list && echo "deb http://deb.debian.org/debian stretch main contrib non-free\ndeb-src http://deb.debian.org/debian stretch main contrib non-free\n\ndeb http://deb.debian.org/debian-security/ stretch/updates main contrib non-free\ndeb-src http://deb.debian.org/debian-security/ stretch/updates main contrib non-free\n\ndeb http://deb.debian.org/debian stretch-updates main contrib non-free\ndeb-src http://deb.debian.org/debian stretch-updates main contrib non-free" > /etc/apt/sources.list && dpkg --add-architecture i386')
+        os.system('adduser %s sudo' %name)#adding user to sudo
+
+    os.system('dpkg --add-architecture i386')#enabling 32 bit support
+
     print('time to (probably) quick update')
     print('ignore apt warnings')
-    time.sleep(4)
-    os.system('sudo apt update && sudo apt upgrade -y')
+    time.sleep(3)
+
+    os.system('sudo apt update && sudo apt upgrade -y')#updating
     print('\033[93mok updates done now time to install some important packages\033[0m')
-    os.system('sudo apt install -y wget bleachbit preload arc-theme moka-icon-theme firmware-linux clamav gdebi needrestart debsums synaptic gksu fail2ban debsecan ufw clamav auditd sysstat')
+    os.system('sudo apt install -y needrestart debsums gksu debsecan ufw clamav gnome-tweak-tool')
+    c()
     print('done\nnow debian-purge will setup firewall')
+    time.sleep(2)
     os.system('sudo ufw default deny incoming && sudo ufw default allow outgoing && ufw enable')
-    nig = raw_input("ok some info u want to say to users who will see on your computer (nothing to continue) : ")
-    if nig != "":
-        os.system('echo "%s" > /etc/issue' %nig)
-        os.system('echo "%s" > /etc/issue.net' %nig)
+    c()
+    issue = input("ok some info u want to say to users who will see on your computer (nothing to continue) : ")
+    if issue != "":
+        os.system('echo "%s" > /etc/issue' %issue)
+        os.system('echo "%s" > /etc/issue.net' %issue)
+    c()
+
     os.system('apt autoremove && apt autocean')
-    print('ok so now time for virus scan (yep on linux it is good to do it)')
-    print('but before need to update virus definitions db\nwait...')
-    os.system('sudo freshclam')
-    print('scanning...')
-    os.system('clamscan --bell --recursive -i --follow-dir-symlinks=2 --detect-pua=yes --algorithmic-detection=yes /')
-    print('done')
+    c()
+
+    print('ok so now time for virus scan (yep on linux it is good to do it)')#sometimes
+    print('but before need to update virus definitions db')
+    time.sleep(2)
+    c()
+
+    print("wait...")
+    os.system('sudo freshclam > /dev/null')
+    c()
+
+    os.system('clamscan --bell --recursive -i --follow-dir-symlinks=2 --detect-pua=yes --algorithmic-detection=yes / & > /dev/null')
+
     print('now some better icons than default...')
-    os.system("sudo apt install -y apt-lisbugs")
-    os.system('wget https://launchpadlibrarian.net/383884507/paper-icon-theme_1.5.721-201808151353~daily~ubuntu18.04.1_all.deb > /dev/null')
-    os.system('dpkg -i paper* && sudo apt install -f && rm paper*')
-    print('change icons in apperance menu or in tweaks')
-    os.system('reset && history -c')
-    print('done enjoy :)')
+    os.system('dpkg -i .assets/paper-icon.deb && sudo apt install -fy')#installing icons
+    c()
+
+    print("Done change icons in tweaks")
 debian()
--- update.sh
+++ update.sh
@@ -1,14 +1,22 @@
-echo "time to update your debian and clean it :)"
-sudo dpkg --add-architecture i386
+echo "time to purge your beautiful, lightweight system" #it's as well incredible, clean, easy to use and lovely system
+
+sudo dpkg --add-architecture i386 # adding 32 bit support 
 sudo apt update > /dev/null
-sudo apt install -fy
+sudo apt install clamav -fy #fixing depends errors
+
+#vvvvvvvvvvvvvvvvvvvvvvvv purging
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove
 sudo apt autoclean
 sudo apt clean
-sudo freshclam > /dev/null
-sudo clamscan --quiet --recursive --remove --detect-pua=yes / &
-echo "done"
+clear
+#^^^^^^^^^^^^^^^^^^^^^^^ purging
+
+sudo freshclam > /dev/null #updates malware db
+sudo clamscan --quiet --recursive --remove --detect-pua=yes / & > /dev/null #scans system in background
+
+clear
+echo "done" #really enjoy it 
 echo "enjoy"
